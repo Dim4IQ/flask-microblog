f8b97852fa93134c07b50688342bf72cde800bec
Dim4IQ <dim4iq@gmail.com>
2024-04-05 16:58:07 +0300

    profile page and avatars

--- app/models.py
+++ app/models.py
@@ -5,12 +5,15 @@ from flask_login import UserMixin
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db, login
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
@@ -22,6 +25,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=robohash&s={size}'
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
