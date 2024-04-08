cc02abe7dcc5628cbea35d6a33bba8cbbb09a227
Dim4IQ <dim4iq@gmail.com>
2024-04-08 18:48:25 +0300

    error handling

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.form import _Auto
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 import sqlalchemy as sa
@@ -32,4 +33,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Save')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session().scalar(sa.select(User).where(User.username == self.username.data))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
 
